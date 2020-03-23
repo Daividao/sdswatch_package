afc53fc3df9edb3f775ab8bad80a81ba07517657
Dao Dai Vi Tran <vitrandao@gmail.com>
2020-03-23 16:37:27 -0700

    configure setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setup(
+  name = "sdswatch",
+  version = "0.0.1",
+  packages = setuptools.find_packages(),
+  entry_points = {
+    "console_scripts" : ['sdswatchagent = sdswatch.sdswatchagent:main']
+  },
+  author = "David Tran",
+  author_email = "vitrandao@gmail.com",
+  description = "SDS Watch",
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
+  python_requires = '>=3.0',
+}
+
+  
