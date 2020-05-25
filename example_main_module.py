4e783f1069961e05f0a5f6570266417dcef2583f
Dao Dai Vi Tran <vitrandao@gmail.com>
2020-05-24 19:01:38 -0700

    reset

--- example_main_module.py
+++ example_main_module.py
@@ -4,7 +4,6 @@ from example_auxiliary_module import download
 sdsw_logger.configure_pge_logger("/Users/trandaod/Desktop/jpl/", "example_hello_world")
 
 if __name__ == "__main__":
-  print("ok")
   sdsw_logger.log("key1", "value1")
   download()
   sdsw_logger.log("key2", "value2")
