0a12cd22a83ba26240a71c7e63389b85c84b98db
Dao Dai Vi Tran <vitrandao@gmail.com>
2020-05-28 22:53:05 -0700

    add documentation

--- onclient/sdswatch-client/sdswatch-client.sh
+++ onclient/sdswatch-client/sdswatch-client.sh
@@ -4,18 +4,14 @@
 docker stop sdswatch-client
 docker rm sdswatch-client
 
-# create a docker container with logstash
-# use --config.reload.automatic to make sure that everytime
-# logstash configuration is updated, logstash will restart 
-# the pipeline
-docker run --name sdswatch-client \
+docker run --name sdswatch-client \ # give a name to the container
 --rm \
--u $(id -u):$(id -g) \
---env HOST=$(curl ifconfig.me) \
--v /data/work/jobs:/jobs \
--v /export/home/hysdsops/verdi/log:/verdi/ \
--v /export/home/hysdsops/verdi/etc/:/usr/share/logstash/config/conf/ \
+-u $(id -u):$(id -g) \ # give the container permission to write to data directory
+--env HOST=$(curl ifconfig.me) \ # create HOST environment variable in the container with value of host machine's ip address
+-v /data/work/jobs:/jobs \ # mount the /data/work/jobs in host machine to /jobs in the container so they share the same thing
+-v /export/home/hysdsops/verdi/log:/verdi/ \ 
+-v /export/home/hysdsops/verdi/etc/:/usr/share/logstash/config/conf/ \ # mount /export/home/hysdsops/verdi/etc/ instead of logstash.conf
 -v /export/home/hysdsops/verdi/share/sdswatch-client/data:/usr/share/logstash/data \
 logstash:7.1.1 \
-logstash -f /usr/share/logstash/config/conf/logstash.conf --config.reload.automatic
+logstash -f /usr/share/logstash/config/conf/logstash.conf --config.reload.automatic # --config.reload.automatic will restart logstash when config file is updated
 
