3d8131159d3fc355d6297685dfd1e6629a5e4024
Dao Dai Vi Tran <vitrandao@gmail.com>
2020-05-28 21:38:34 -0700

    add documentation

--- onserver/sdswatch-server/sdswatch-server.sh
+++ onserver/sdswatch-server/sdswatch-server.sh
@@ -1,9 +1,17 @@
 #!/usr/bin bash
 
+# stop sdswatch-logstash, sdswatch-elasticsearch,
+# sdswatch-redis, load-kibana-dasbhboard if it they exist.
+# This is a bad syntax because I stop everything
 docker stop $(docker ps -q -a)
 docker rm $(docker ps -q -a)
+
+# re-download images
+# reason: If I don't do it, I got a very weird error.
 docker rmi docker.elastic.co/logstash/logstash:7.1.1
 docker rmi docker.elastic.co/kibana/kibana:7.1.1
 docker rmi docker.elastic.co/elasticsearch/elasticsearch:7.1.1 
+
+
 docker-compose build
 docker-compose up
